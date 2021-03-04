789850a7c71ccbee46cb7117402e0f237c6b8208
Plasius <Plasius@users.noreply.github.com>
2021-03-04 18:07:01 +0200

    Scraper update - input beolvasás

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,7 +4,60 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver=webdriver.Chrome("chromedriver.exe")
+driver = webdriver.Chrome("chromedriver.exe")
+
+def read_input():
+	#txt sorainak ertelmezese
+	keywords_line = 1
+	location_line = 2
+	date_posted_start,date_posted_end = 5, 9
+	experience_level_start,experience_level_end = 11, 17
+	job_type_start,job_type_end = 19, 26
+	remote_line = 27
+	easy_apply_line = 28
+	under_ten_applicants_line = 29
+	company_line = 31
+	industry_line = 32
+	job_function_line = 33
+
+	#txt beolvasása
+	input_file=open('szures.txt', encoding="utf8")
+	input = [line.strip().split() for line in input_file]
+
+	#search & location string
+	keywords = ' '.join(input[keywords_line])
+	location = ' '.join(input[location_line])
+
+	#csak egy érték lehet
+	date = [date[0].strip(date[0][-1]).replace('_',' ') for date in input[date_posted_start:date_posted_end] if len(date)>1]
+	
+	#több érték is lehet
+	experience_levels_list = [level[0].strip(level[0][-1]).replace('_',' ') for level in input[experience_level_start:experience_level_end] if len(level) > 1]
+	job_types_list = [type[0].strip(type[0][-1]) for type in input[job_type_start:job_type_end] if len(type)>1]
+ 
+	#true or false
+	is_remote = True if input[remote_line][-1]=='Y' else False
+	is_easy_apply = True if input[easy_apply_line][-1]=='Y' else False   
+	is_under_ten = True if input[under_ten_applicants_line][-1]=='Y' else False
+
+	#listak
+	companies_list = ' '.join(input[company_line][1:]).split(', ')
+	industries_list = ' '.join(input[industry_line][1:]).split(', ')
+	job_functions_list = ' '.join(input[job_function_line][1:]).split(', ')
+
+	#print
+	print(keywords)
+	print(location)
+	print(date)
+	print(experience_levels_list)
+	print(job_types_list)
+	print(is_remote)
+	print(is_easy_apply)
+	print(is_under_ten)
+	print(companies_list)
+	print(industries_list)
+	print(job_functions_list)
+
 
 def login():
 	#chrome megnyitasa
@@ -26,6 +79,7 @@ def login():
 	password.send_keys("bitfakeprofile42")
 	password.send_keys(Keys.RETURN)
 
-	
+
+read_input()	
 login()
 driver.get("https://www.linkedin.com/jobs")
