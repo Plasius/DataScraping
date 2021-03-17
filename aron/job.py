71f749de2b276aa83e515393d6608f16e53a750f
BaloghAron2066 <balogharon2066@gmail.com>
2021-03-17 12:21:21 +0100

    "Job" osztály megírása

---
+++ aron/job.py
@@ -0,0 +1,20 @@
+#állásajánlat adatai
+class job:
+    #13db adat van
+    def __init__(self, megnevezes, ceg, varos, jelentkezok, org, company, xplvl, type, slvl, industry, emptype, functions, szoveg):
+        self.megnevezes=megnevezes
+        self.ceg=ceg
+        self.varos=varos
+        self.jelentkezok=jelentkezok
+        self.org=org
+        self.company=company
+        self.xplvl=xplvl
+        self.type=type
+        self.slvl=slvl
+        self.industry=industry
+        self.emptype=emptype
+        self.functions=functions
+        self.szoveg=szoveg
+
+
+
