f172e643bd270969b244dacdc486ccc9081d8d38
birosbeni <Beni>
2021-11-04 19:46:06 +0100

    linkedIn job

--- scraper/LinkedinJob.py
+++ scraper/LinkedinJob.py
@@ -1,13 +1,12 @@
 #állásajánlat adatai
 class LinkedinJob:
-    def __init__(self, title, company, location, seniority, employment_type, industry_list, job_functions_list):
+    def __init__(self, title, company, location, experience_level, job_type, industry):
         self.title=title
         self.company=company
         self.location=location
-        self.seniority=seniority
-        self.employment_type=employment_type
-        self.industry_list=industry_list
-        self.job_functions_list=job_functions_list
+        self.experience_level=experience_level
+        self.job_type=job_type
+        self.industry=industry
 
     def __str__(self):
-        return self.title + self.company + self.location+self.seniority + self.employment_type + str(self.industry_list) + str(self.job_functions_list)
+        return self.title + self.company + self.location+self.experience_level + self.job_type + str(self.industry)
